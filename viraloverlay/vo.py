cc63b371e27bd289188a72bb732b72221f5261a4
zevav <zev@averba.ch>
2019-03-08 21:59:46 -0500

    removed support for transcripts as strings at CLI

--- viraloverlay/vo.py
+++ viraloverlay/vo.py
@@ -10,20 +10,15 @@ from .viraloverlay import ViralOverlay
 @click.option('-g', '--gif', is_flag=True, help='output to GIF instead of MP4')
 @click.option('-f', '--font-path', help='path to the font you\'d like to use.')
 @click.argument('filepath', type=click.Path(exists=True))
-@click.argument('overlay_data', type=str)
-def cli(filepath, overlay_data, gif, font_path):
+@click.argument('overlay_data_path', type=click.File('r'))
+def cli(filepath, overlay_data_path, gif, font_path):
     """
     Creates a new video with text overlaid on it.
 
        OVERLAY_DATA: a JSON file path OR a JSON-formatted string containing an array of overlays, each containing key-value pairs of at least "text", "start", and "stop". optional keys: "font_size", "font_color", "text_position_x", and "text_position_y"
     """
-    if overlay_data.endswith('.json'):
-        if not Path(overlay_data).exists():
-            raise click.ClickException(f'{overlay_data} doesn\'t exist')
-        with open(overlay_data) as fin:
-            overlay_data_json = json.load(fin)
-    else:
-        overlay_data_json = tuple(json.loads(overlay_data))
+    overlay_data_file_handle = overlay_data_path
+    overlay_data_json = json.load(overlay_data_file_handle)
 
     v = ViralOverlay(filepath, overlays=overlay_data_json, font_path=font_path)
     method = v.gif if gif else v.go
